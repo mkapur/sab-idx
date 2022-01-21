87e1959bdc1c3c0af0965790a8a24c5f2c5f82fd
Maia Sosa Kapur <kapurm@uw.edu>
2022-01-21 09:23:26 -0800

    Update 2a_run500_WConly.R

--- R/2a_run500_WConly.R
+++ R/2a_run500_WConly.R
@@ -6,22 +6,22 @@ library(ggplot2)
 
 packageVersion('VAST')
 packageVersion('FishStatsUtils')
-Species <- "Anoplopoma fimbria"
+
 # Load the data for VAST
 Data_Geostat <- readRDS(file =  here('data','2022-01-14inputVast.rds')) %>% 
   filter(Survey %in% c('NWFSC_Combo',"Triennial_late" , "Triennial_early"))
 
-## from v3
+## as in VASTWestCoast
 FieldConfig = matrix( c("Omega1"=1, "Epsilon1"=1, "Omega2"=1, 
                         "Epsilon2"=1, "Beta1"="IID", "Beta2"="IID"), nrow=3, byrow=TRUE )
-RhoConfig = c("Beta1"=3, "Beta2"=3, "Epsilon1"=2, "Epsilon2"=2) 
+RhoConfig = c("Beta1"=0, "Beta2"=0, "Epsilon1"=0, "Epsilon2"=0) 
 
 strata.limits = data.frame('STRATA' = c('C1','C2'), 'north_border' = c(36,50), 'south_border' = c(25,36))
 # Make settings 
 settings <- make_settings( Version = "VAST_v12_0_0",
                           n_x = 500,#1000, 
                           Region =  "california_current",
-                          purpose = "index2", #index 2 is recommended BUT doesn't provide annual estimates
+                          purpose = "index2", 
                           fine_scale = TRUE, 
                           ObsModel= c(2,0), #c(2,1), #c(1,1) #c(10,2)
                           strata.limits=strata.limits,
@@ -30,7 +30,7 @@ settings <- make_settings( Version = "VAST_v12_0_0",
                           FieldConfig = FieldConfig,
                           knot_method = "grid", 
                           bias.correct = TRUE,
-                          vars_to_correct = list(vars_to_correct = "Index_cyl"),
+                          # vars_to_correct =  c("Index_cyl"),
                           use_anisotropy = TRUE)
 gc()
 
