256976400dc0bd6159bd359e6af8eac232c4ffc2
Maia Sosa Kapur <kapurm@uw.edu>
2022-01-28 13:08:23 -0800

    revert to v12, manually separate early years
    The version with the ObsModel configured like RACE recommended (2,1) looks great in later years, but is off by a weird order of magnitude (kg/100 not 1000) and also too high in early years. Doing test run where data before 1990 are treated as separate fleet, can also try just dropping those years. Runs on Jan 15 and 27 had those years dropped but also excluded data > 500 m depth, when we want to truncate at 700m.

--- R/2b_run500_AKonly.R
+++ R/2b_run500_AKonly.R
@@ -9,6 +9,7 @@ packageVersion('FishStatsUtils')
 # Load the data for VAST
 Data_Geostat <- readRDS(file =  here('data','2022-01-28inputVast.rds')) %>%  
   filter(Survey %in% c('GOA_LATE','GOA_EARLY') )
+Data_Geostat$Survey[Data_Geostat$Year < 1990] <- "Dropped_Years"
 
 FieldConfig = matrix( c("IID","IID","IID","IID","IID","IID"), ncol=2, nrow=3, dimnames=list(c("Omega","Epsilon","Beta"),c("Component_1","Component_2")) )
 RhoConfig  = c("Beta1" = 0, "Beta2" = 0, "Epsilon1" = 0, "Epsilon2" = 0)
@@ -16,7 +17,7 @@ RhoConfig  = c("Beta1" = 0, "Beta2" = 0, "Epsilon1" = 0, "Epsilon2" = 0)
 strata.limits = data.frame('STRATA' = c('A4','A3'), 'west_border' = c(-Inf,-146), 'east_border' = c(-146,-130))
 
 # Make settings 
-settings <- make_settings( Version = "VAST_v8_5_0",
+settings <- make_settings( Version = "VAST_v12_0_0",
                            n_x = 500, 
                            Region =  "gulf_of_alaska",
                            purpose = "index2", 
@@ -31,7 +32,7 @@ settings <- make_settings( Version = "VAST_v8_5_0",
 gc()
 
 
-wkdir <-  here('runs',paste0(Sys.Date(),"-AK_500-146-v8_5/"))
+wkdir <-  here('runs',paste0(Sys.Date(),"-AK_500-146-v12-sep80s/"))
 dir.create(wkdir)
 # Run model
 fit <- fit_model( "settings"=settings, 
@@ -42,7 +43,7 @@ fit <- fit_model( "settings"=settings,
                   "a_i"=Data_Geostat[,'AreaSwept'], 
                   "v_i"=Data_Geostat[,'Vessel'], 
                   optimize_args=list("lower"=-Inf,"upper"=Inf),
-                  "working_dir" =wkdir)
+                  "working_dir" =paste0(wkdir,"/"))
 # Plot results
 plot( fit )
 
