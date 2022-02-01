a925fe0ea20ab20a8ef6e797c02bc7cd3e7c1415
Maia Sosa Kapur <kapurm@uw.edu>
2022-02-01 10:44:43 -0800

    re-running with new settings
    as of today the run on 1/15 is the only one that's remotely in line/scale with the recent assessment. The latest runs have a weird scale.

--- R/2b_run500_AKonly.R
+++ R/2b_run500_AKonly.R
@@ -7,11 +7,14 @@ library(ggplot2)
 packageVersion('VAST')
 packageVersion('FishStatsUtils') 
 # Load the data for VAST
-Data_Geostat <- readRDS(file =  here('data','2022-01-28inputVast.rds')) %>%  
-  filter(Survey %in% c('GOA_LATE','GOA_EARLY') )
-Data_Geostat$Survey[Data_Geostat$Year < 1990] <- "Dropped_Years"
+Data_Geostat <- readRDS(file =  here('data','2022-02-01inputVast.rds')) %>%  
+  filter(Survey %in% c('GOA_LATE','GOA_EARLY') & Year < 2021) 
+# Data_Geostat$Survey[Data_Geostat$Year < 1990] <- "Dropped_Years"
+# Data_Geostat$Survey[Data_Geostat$Year < 1990] <- "Dropped_Years"
 
-FieldConfig = matrix( c("IID","IID","IID","IID","IID","IID"), ncol=2, nrow=3, dimnames=list(c("Omega","Epsilon","Beta"),c("Component_1","Component_2")) )
+
+FieldConfig = matrix( c("IID","IID","IID","IID","IID","IID"), ncol=2, nrow=3, 
+                      dimnames=list(c("Omega","Epsilon","Beta"),c("Component_1","Component_2")) )
 RhoConfig  = c("Beta1" = 0, "Beta2" = 0, "Epsilon1" = 0, "Epsilon2" = 0)
 
 strata.limits = data.frame('STRATA' = c('A4','A3'), 'west_border' = c(-Inf,-146), 'east_border' = c(-146,-130))
@@ -32,7 +35,7 @@ settings <- make_settings( Version = "VAST_v12_0_0",
 gc()
 
 
-wkdir <-  here('runs',paste0(Sys.Date(),"-AK_500-146-v12-sep80s/"))
+wkdir <-  here('runs',paste0(Sys.Date(),"-AK_500-146-v12-500m/"))
 dir.create(wkdir)
 # Run model
 fit <- fit_model( "settings"=settings, 
