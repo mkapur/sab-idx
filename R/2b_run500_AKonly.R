3aad9919172db9f2133362173afafddb48c151ce
Maia Sosa Kapur <kapurm@uw.edu>
2022-05-03 11:41:24 -0700

    Update 2b_run500_AKonly.R

--- R/2b_run500_AKonly.R
+++ R/2b_run500_AKonly.R
@@ -8,7 +8,7 @@ packageVersion('VAST')
 packageVersion('FishStatsUtils') 
 # Load the data for VAST
 Data_Geostat <- readRDS(file =  here('data','2022-02-01inputVast.rds')) %>%  
-  filter(Survey %in% c('GOA_LATE','GOA_EARLY') & Year < 2021) 
+  filter(Survey %in% c('GOA_LATE','GOA_EARLY') & Year >= 1990) 
 # Data_Geostat$Survey[Data_Geostat$Year < 1990] <- "Dropped_Years"
 # Data_Geostat$Survey[Data_Geostat$Year < 1990] <- "Dropped_Years"
 
@@ -17,10 +17,10 @@ FieldConfig = matrix( c("IID","IID","IID","IID","IID","IID"), ncol=2, nrow=3,
                       dimnames=list(c("Omega","Epsilon","Beta"),c("Component_1","Component_2")) )
 RhoConfig  = c("Beta1" = 0, "Beta2" = 0, "Epsilon1" = 0, "Epsilon2" = 0)
 
-strata.limits = data.frame('STRATA' = c('A4','A3'), 'west_border' = c(-Inf,-146), 'east_border' = c(-146,-130))
+strata.limits = data.frame('STRATA' = c('A4','A3'), 'west_border' = c(-Inf,-145), 'east_border' = c(-145,-130))
 
 # Make settings 
-settings <- make_settings( Version = "VAST_v12_0_0",
+settings <- make_settings( Version = "VAST_v13_1_0",
                            n_x = 500, 
                            Region =  "gulf_of_alaska",
                            purpose = "index2", 
@@ -35,7 +35,7 @@ settings <- make_settings( Version = "VAST_v12_0_0",
 gc()
 
 
-wkdir <-  here('runs',paste0(Sys.Date(),"-AK_500-146-v12-500m/"))
+wkdir <-  here('runs',paste0(Sys.Date(),"-AK_500-145-v13-1-500m-obs21-drop80s/"))
 dir.create(wkdir)
 # Run model
 fit <- fit_model( "settings"=settings, 
